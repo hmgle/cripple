6b44c6419902d6f791fa0bbc51ab22721707ec1f
mingang.he <dustgle@gmail.com>
2015-04-25 23:51:37 +0800

    update client_getchangeip_test.c

--- client_getchangeip_test.c
+++ client_getchangeip_test.c
@@ -9,6 +9,24 @@
 #include <unistd.h>
 #include <string.h>
 
+static ssize_t set_changed_request_attr(uint8_t *buf_end)
+{
+	buf_end[0] = 0x0;
+	buf_end[1] = 0x01;
+	buf_end[2] = 0x0;
+	buf_end[3] = 0x08;
+	RAND_bytes(buf_end + 4, 16);
+	buf_end[20] = 0x0;
+	buf_end[21] = 0x03;
+	buf_end[22] = 0x0;
+	buf_end[23] = 0x04;
+	buf_end[24] = 0x0;
+	buf_end[25] = 0x0;
+	buf_end[26] = 0x0;
+	buf_end[27] = 0x06;
+	return 28;
+}
+
 int main(int argc, char **argv)
 {
 	int cli_fd;
@@ -47,6 +65,14 @@ int main(int argc, char **argv)
 	len = recvfrom(cli_fd, buf, STUN_MAX_MESSAGE_SIZE, 0,
 			&source_addr, &fromlen);
 	int i;
+	for (i = 0; i < len; i++) {
+		fprintf(stderr, "%d: %#4x\n", i, buf[i]);
+	}
+
+	len = set_changed_request_attr(buf);
+	sendto(cli_fd, buf, len, 0, (struct sockaddr *)&to, tolen);
+	len = recvfrom(cli_fd, buf, STUN_MAX_MESSAGE_SIZE, 0,
+			&source_addr, &fromlen);
 	for (i = 0; i < len; i++) {
 		fprintf(stderr, "%d: %#4x\n", i, buf[i]);
 	}
